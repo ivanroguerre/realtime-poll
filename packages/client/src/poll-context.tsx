83635e938383bfaa4127f1c43941bef4eb6f5477
Iván Guerrero <ivanguebri@gmail.com>
2024-01-08 10:13:24 -0500

    Add action to delete a poll item

--- packages/client/src/poll-context.tsx
+++ packages/client/src/poll-context.tsx
@@ -4,6 +4,7 @@ type PollContextType = {
   pollItems: string[];
   actions: {
     addPollItem: (pollItem: string) => void;
+    deletePollItem: (pollItem: string) => void;
   };
 };
 
@@ -13,9 +14,15 @@ const PollContextProvider = ({ children }: PropsWithChildren): JSX.Element => {
   const [pollItems, setPollItems] = useState<string[]>([]);
   const addPollItem = (pollItem: string) =>
     setPollItems((pollItems) => pollItems.concat(pollItem));
+  const deletePollItem = (toDeletePollItem: string) =>
+    setPollItems((pollItems) =>
+      pollItems.filter((pollItem) => pollItem !== toDeletePollItem)
+    );
 
   return (
-    <PollContext.Provider value={{ pollItems, actions: { addPollItem } }}>
+    <PollContext.Provider
+      value={{ pollItems, actions: { addPollItem, deletePollItem } }}
+    >
       {children}
     </PollContext.Provider>
   );
